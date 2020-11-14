aae0d8545bbfdfb23fc1d03b58dc6c7e32cc7dc6
Anna Bychkova <bychkova.dev@gmail.com>
2020-11-14 16:54:38 +0200

    renders one page of images

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,18 +1,18 @@
-// const BASE_URL = 'https://pixabay.com';
+const BASE_URL = 'https://pixabay.com';
 
-// let page = 0;
+let page = 0;
 
-// function fetchImages(searchQuery) {
-//   page += 1;
-//   console.log(page);
-//   return fetch(
-//     `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=19110749-e340c63922b3f8a4d502270f7`,
-//   ).then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-//     throw new Error();
-//   });
-// }
+function fetchImages(searchQuery) {
+  page += 1;
+  console.log(page);
+  return fetch(
+    `${BASE_URL}/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=19110749-e340c63922b3f8a4d502270f7`,
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error();
+  });
+}
 
-// export default fetchImages;
+export default fetchImages;
--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,10 +1,54 @@
 import API from './apiService';
 import '../css/styles.css';
+import debounce from 'lodash.debounce';
+import imageCardTpl from '../templates/image-card.hbs';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, info, error, defaultModules } from '@pnotify/core';
 
-// API('flower').then(r => console.log(r));
+const inputEl = document.querySelector('input[name="query"]');
+const galleryEl = document.querySelector('.js-gallery-container');
+const loadMoreButtonEl = document.querySelector('.load-more-button');
+let searchQuery;
+galleryEl.innerHTML = '';
+inputEl.addEventListener('input', debounce(onInputType, 500));
 
-fetch(
-  'https://pixabay.com/api/?key=19110749-e340c63922b3f8a4d502270f7&q=yellow+flowers&image_type=photo',
-)
-  .then(result => console.log(result.json))
-  .then(console.log);
+console.log(loadMoreButtonEl);
+// loadMoreButtonEl.addEventListener('click', onButtonClick);
+
+function onInputType(e) {
+  e.preventDefault();
+  const field = e.target;
+  galleryEl.innerHTML = '';
+  searchQuery = inputEl.value;
+  API(searchQuery)
+    .then(renderImageCards)
+    .catch(onFetchError)
+    .finally(() => field.reset);
+}
+
+function renderImageCards({ hits }) {
+  console.log(hits);
+  galleryEl.innerHTML = imageCardTpl(hits);
+}
+
+function onFetchError() {
+  console.log('Something went wrong');
+  alert({
+    text: 'Something went wrong',
+    maxTextHeight: null,
+    sticker: false,
+  });
+}
+
+function onButtonClick(e) {
+  console.log('button click');
+  // e.preventDefault();
+  // const field = e.target;
+  // galleryEl.innerHTML = '';
+  // searchQuery = inputEl.value;
+  // API(searchQuery)
+  //   .then(renderImageCards)
+  //   .catch(onFetchError)
+  //   .finally(() => field.reset);
+}
